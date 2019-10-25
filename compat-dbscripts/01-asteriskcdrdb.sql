5015fa3d5d08849f638f21fca5f91a5dae2aede3
Nicolas Gudino <asternic@gmail.com>
2019-10-25 14:05:49 -0400

    Update cdr schema. Add CEL table

--- compat-dbscripts/01-asteriskcdrdb.sql
+++ compat-dbscripts/01-asteriskcdrdb.sql
@@ -19,33 +19,74 @@
 -- Current Database: `asteriskcdrdb`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `asteriskcdrdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `asteriskcdrdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
 USE `asteriskcdrdb`;
 
 --
 -- Table structure for table `cdr`
 --
-
 DROP TABLE IF EXISTS `cdr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cdr` (
-  `calldate` datetime NOT NULL default '0000-00-00 00:00:00',
-  `clid` varchar(80) NOT NULL default '',
-  `src` varchar(80) NOT NULL default '',
-  `dst` varchar(80) NOT NULL default '',
-  `dcontext` varchar(80) NOT NULL default '',
-  `channel` varchar(80) NOT NULL default '',
-  `dstchannel` varchar(80) NOT NULL default '',
-  `lastapp` varchar(80) NOT NULL default '',
-  `lastdata` varchar(80) NOT NULL default '',
-  `duration` int(11) NOT NULL default '0',
-  `billsec` int(11) NOT NULL default '0',
-  `disposition` varchar(45) NOT NULL default '',
-  `amaflags` int(11) NOT NULL default '0',
-  `accountcode` varchar(20) NOT NULL default '',
-  `uniqueid` varchar(32) NOT NULL default '',
-  `userfield` varchar(255) NOT NULL default ''
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `calldate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `clid` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `src` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `dst` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `dcontext` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `channel` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `dstchannel` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `lastapp` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `lastdata` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `duration` int(11) NOT NULL DEFAULT '0',
+  `billsec` int(11) NOT NULL DEFAULT '0',
+  `disposition` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `amaflags` int(11) NOT NULL DEFAULT '0',
+  `accountcode` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `uniqueid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `userfield` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `recordingfile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `cnum` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `cnam` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `outbound_cnum` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `outbound_cnam` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `dst_cnam` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `did` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  KEY `IDX_UNIQUEID` (`uniqueid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `cel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `eventtype` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eventtime` datetime NOT NULL,
+  `cid_name` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cid_num` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cid_ani` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cid_rdnis` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cid_dnid` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exten` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `context` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `channame` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appname` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appdata` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amaflags` int(11) NOT NULL,
+  `accountcode` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uniqueid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `linkedid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `peer` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userdeftype` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eventextra` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userfield` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uniqueid_index` (`uniqueid`),
+  KEY `linkedid_index` (`linkedid`)
+) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cdr`
